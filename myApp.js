3fd2fec8b2dec25165aa5e0e217d0b4d617dbf7c
WilliamLi <wilheim.ry@gmail.com>
2019-01-14 23:40:54 +1300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 // --> 7)  Mount the Logger middleware here
 
 
